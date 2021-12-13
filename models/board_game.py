a9f90090ce7b20c1beccd65d630db935f99573a0
jhtdod <jess.dodge@hotmail.co.uk>
2021-12-13 10:11:10 +0000

    setup complete

---
+++ models/board_game.py
@@ -0,0 +1,10 @@
+class BoardGame: 
+
+    def __init__(self, name, description, quantity, cost, price, manufacturer, id=None):
+        self.name = name
+        self.description = description
+        self.quantity = quantity
+        self.cost = cost
+        self.price = price
+        self.manufacturer = manufacturer
+        self.id = id
---
+++ models/manufacturer.py
@@ -0,0 +1,6 @@
+class Manufacturer:
+
+    def __init__(self, name, contact_details, id=None):
+        self.name = name
+        self.contact_details = contact_details
+        self.id = id
