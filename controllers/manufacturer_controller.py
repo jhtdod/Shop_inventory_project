f9b52a154c5d37868cebc8db4f6873175e4fbd5f
jhtdod <jess.dodge@hotmail.co.uk>
2021-12-14 12:31:35 +0000

    MVP html done - editing still required

--- controllers/manufacturer_controller.py
+++ controllers/manufacturer_controller.py
@@ -12,39 +12,39 @@ def show_manufacturer():
     return render_template('manufacturers/index.html', manufacturers=manufacturers)
 
 
-@manufacturer_blueprint.route('/manufacturer/<int:id>')
+@manufacturer_blueprint.route('/manufacturers/<int:id>')
 def show_details(id):
     manufacturer = manufacturer_repository.select(id)
-    return render_template('manufacturer/show.html', manufacturer=manufacturer)
+    return render_template('manufacturers/show.html', manufacturer=manufacturer)
 
-@manufacturer_blueprint.route('/manufacturer/add')
+@manufacturer_blueprint.route('/manufacturers/add')
 def show_add_form():
     board_games = board_game_repository.select_all()
-    return render_template('manufacturer/new.html', board_games=board_games)
+    return render_template('manufacturers/new.html', board_games=board_games)
 
-@manufacturer_blueprint.route('/manufacturer/add', methods=['POST'])
+@manufacturer_blueprint.route('/manufacturers/add', methods=['POST'])
 def add_manufacturer():
     name = request.form["name"]
     contact_details = request.form["contact_details"]
     new_manufacturer = Manufacturer(name, contact_details)
     manufacturer_repository.save(new_manufacturer)
-    return redirect('/manufacturer')
+    return redirect('/manufacturers')
 
-@manufacturer_blueprint.route('/manufacturer/edit/<int:id>')
+@manufacturer_blueprint.route('/manufacturers/edit/<int:id>')
 def show_edit(id):
     manufacturer = manufacturer_repository.select(id)
     board_games = board_game_repository.select_all()
-    return render_template('/manufacturer/edit.html', board_games=board_games, manufacturer=manufacturer)
+    return render_template('/manufacturers/edit.html', board_games=board_games, manufacturer=manufacturer)
 
-@manufacturer_blueprint.route('/manufacturer/edit/<int:id>', methods=['POST'])
+@manufacturer_blueprint.route('/manufacturers/edit/<int:id>', methods=['POST'])
 def edit_manufacturer(id):
     name = request.form["name"]
     contact_details = request.form['contact_details']
     updated_manufacturer = Manufacturer(name, contact_details, id)
     manufacturer_repository.update(updated_manufacturer)
-    return redirect('/manufacturer')
+    return redirect('/manufacturers')
 
-@manufacturer_blueprint.route('/manufacturer/delete/<int:id>')
+@manufacturer_blueprint.route('/manufacturers/delete/<int:id>')
 def delete_manufacturer(id):
     manufacturer_repository.delete(id)
-    return redirect('/manufacturer')
+    return redirect('/manufacturers')
