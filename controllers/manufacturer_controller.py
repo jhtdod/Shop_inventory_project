3ea6cc2d3b40f80ca25b9f41d6c70eee8fd30dc3
jhtdod <jess.dodge@hotmail.co.uk>
2021-12-14 12:15:59 +0000

    manufacturer controller updated: edit_game changed to edit_manufacturer, delete_manufacturer added

--- controllers/manufacturer_controller.py
+++ controllers/manufacturer_controller.py
@@ -37,14 +37,14 @@ def show_edit(id):
     return render_template('/manufacturer/edit.html', board_games=board_games, manufacturer=manufacturer)
 
 @manufacturer_blueprint.route('/manufacturer/edit/<int:id>', methods=['POST'])
-def edit_game(id):
+def edit_manufacturer(id):
     name = request.form["name"]
     contact_details = request.form['contact_details']
     updated_manufacturer = Manufacturer(name, contact_details, id)
     manufacturer_repository.update(updated_manufacturer)
     return redirect('/manufacturer')
 
-@board_game_blueprint.route('/inventory/delete/<int:id>')
-def delete_game(id):
-    board_game_repository.delete(id)
-    return redirect('/inventory')
+@manufacturer_blueprint.route('/manufacturer/delete/<int:id>')
+def delete_manufacturer(id):
+    manufacturer_repository.delete(id)
+    return redirect('/manufacturer')
